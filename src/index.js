52751bd0cf8b4abb2b2f84e47c7244907ec251f2
rafalh <rafal.hibner@secom.com.pl>
2023-07-13 11:25:44 +0200

    bump version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import GraphiQL from "graphiql";
-import "graphiql/graphiql.css";
+import "graphiql/graphiql.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
